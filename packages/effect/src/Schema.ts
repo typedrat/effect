3be98088c21cbb3f3a07f1da3d8270d04eaded3c
Sebastian Lorenz <fubhy@fubhy.com>
2026-08-31 10:00:33 +0200

    use schema tagged net address errors

--- packages/effect/src/Schema.ts
+++ packages/effect/src/Schema.ts
@@ -12057,7 +12057,7 @@ export interface Duration extends declare<Duration_.Duration> {
 
 const netAddressFromString = <A>(
   declaration: declare<A>,
-  parse: (input: string) => Result_.Result<A, InternalNetAddress.NetAddressError>,
+  parse: (input: string) => Result_.Result<A, InternalNetAddress.NetAddressIssue>,
   encode: (value: A) => string
 ) =>
   String.pipe(decodeTo(
